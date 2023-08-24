4e6ba522c194b02647fc74f18b154e22740c882e
Avik2109 <avikdey1309@gmail.com>
2023-08-24 17:42:31 +0530

    Commit Test2.java to BranchX - First Time

---
+++ src/git/java/demo/Test2.java
@@ -0,0 +1,18 @@
+package git.java.demo;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		String hungry = "Yes";
+		
+		if (hungry=="Yes") {
+			
+			System.out.println("Please eat something");
+		}
+		else {
+			System.out.println("Don't eat anything");
+		}
+	}
+
+}
